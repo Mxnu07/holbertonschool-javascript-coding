bb490768519f88eaea4c47617316faef971650c0
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-28 16:02:39 -0400

    adding packages

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,23 +1,13 @@
 /* eslint-disable */
-import express from 'express';
+const express = require('express');
+const router = express.Router();
+
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+router.get('/', (req, res) => AppController.getHomepage(req, res));
+router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
+router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
 
-  router.get('/', (req, res) => {
-    AppController.getHomepage(req, res);
-  });
-
-  router.get('/students', (req, res) => {
-    StudentsController.getAllStudents(req, res, process.argv[2]);
-  });
-
-  router.get('/students/:major', (req, res) => {
-    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
-  });
-}
-
-export default controllerRouting;
+export default router;
+module.exports = router;
